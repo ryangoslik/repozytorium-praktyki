b316ee66e3b4416b40bd9f1e47beabe50bd165cb
ryangoslik <konrad.bienkowski1@o2.pl>
2024-12-09 13:53:19 +0100

    123

--- gra/gra/gra/gra.cpp
+++ gra/gra/gra/gra.cpp
@@ -160,7 +160,7 @@ void tankPrologue(Player& player) {
     typeWriterEffect("3. Nie podejmuj zadnej akcji...");
 
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
- 
+
     std::cin >> choice;
 
     if (choice == 1) {
@@ -294,16 +294,37 @@ void tankPrologue(Player& player) {
                     player.tank.fire("AP");
                     typeWriterEffect("Cel zniszczony! - Krzyknal Brad");
                     typeWriterEffect("Nastepny tuz przed nami!! - Krzyknal John");
-                    player.tank.fire("AP");
-                    typeWriterEffect("Rykoszet! - Krzyknal Robert");
+                    typeWriterEffect("1. Strzel z AP w czolg przeciwnika");
+                    typeWriterEffect("2. Nic nie rob");
+
+                    int choice; cin >> choice;
+                    if (choice == 1) {
+                        player.tank.fire("AP");
+                        typeWriterEffect("Rykoszet! - Krzyknal Robert");
+                    }
+                    else {
+                        player.health = 0;
+                        return;
+                    }
+
                     if (player.tank.ammoAP > 0) {
                         typeWriterEffect("Laduj AP!");
-                        typeWriterEffect("Strzelaj bez rozkazu "+player.name+"!!!");
+                        typeWriterEffect("Strzelaj bez rozkazu " + player.name + "!!!");
                         player.tank.fire("AP");
-                    }else{
+                    }
+                    else {
                         typeWriterEffect("Brak amunicji AP! Zaladuj HVAP!!");
-                        player.tank.fire("HVAP");
-                        typeWriterEffect("Trafienie w wieze! Unieszkodliwieni!! Krzyczy Mike");
+                        typeWriterEffect("1. Strzel z HVAP w czolg przeciwnika");
+                        typeWriterEffect("2. Nic nie rob");
+                        int choice; cin >> choice;
+                        if (choice == 1) {
+                            player.tank.fire("HVAP");
+                            typeWriterEffect("Trafienie w wieze! Unieszkodliwieni!! Krzyczy Mike");
+                        }
+                        else {
+                            player.health = 0;
+                            return;
+                        }
                         typeWriterEffect("Brad, " + player.name + "! Strzelajcie z km-ow w piechote! - Krzyknal Early");
                     }
                     typeWriterEffect("Dwa tygrysy wyjechaly z 3 uwaga! - Krzyknal ze strachem Mike");
@@ -312,8 +333,8 @@ void tankPrologue(Player& player) {
                     typeWriterEffect("*Zostalismy trafieni w bok wiezy, widzialem same iskry*");
                     typeWriterEffect("*Niemieccy zolnierze wyciagli mnie i Brada z naszego czolgu*");
                     typeWriterEffect("*Mieli jakies plecaki, mnostwo jakichs laczy, kabli, nigdy nie widzialem czegos podobnego*");
-                    typeWriterEffect("*Jestem ledwo przytomny ale widze go, Brada, ciagna go po ziemi do jakiegos transportera*");
-                }      
+                    typeWriterEffect("*Jestem ledwo przytomny ale widze go, Brada, ciagna go po ziemi do jakiegos transportera, jest 9 grudnia 45 roku, wojna ma sie skonczyc jutro, chociaz miala sie.*\nNacisnij 1 aby kontynuowac");
+                }
             }
             else if (choice == 2) {
                 typeWriterEffect("Brad, " + player.name + "! Strzelajcie z km-ow w piechote! - Krzyknal Early");
@@ -321,25 +342,151 @@ void tankPrologue(Player& player) {
                 typeWriterEffect("*Zostalismy trafieni w bok wiezy, widzialem same iskry*");
                 typeWriterEffect("*Niemieccy zolnierze wyciagli mnie i Brada z naszego czolgu*");
                 typeWriterEffect("*Mieli jakies plecaki, mnostwo jakichs laczy, kabli, nigdy nie widzialem czegos podobnego*");
-                typeWriterEffect("*Jestem ledwo przytomny ale widze go, Brada, ciagna go po ziemi do jakiegos transportera*");
+                typeWriterEffect("*Jestem ledwo przytomny ale widze go, Brada, ciagna go po ziemi do jakiegos transportera, jest 9 grudnia 45 roku, wojna ma sie skonczyc jutro, chociaz miala sie.*\nNacisnij 1 aby kontynuowac");
             }
             else {
                 typeWriterEffect("Nieprawidlowy wybor! Strata czasu, przeciwnik atakuje!");
-                player.health = 0; 
-            
+                player.health = 0;
+                return;
             }
-           
+
+        }
+    }
+
+
+    if (player.health > 0) {
+        int choice;
+        typeWriterEffect("\n=== Rozdzial 1: " + player.unit + " ===");
+        typeWriterEffect("Rok 1945. Po latach brutalnych zmagan na frontach Europy, II wojna swiatowa miala zakonczyc sie w inny sposob. Alianci, wspierani przez komunistyczne sil ZSRR, przeprowadzili ostatnia, desperacka ofensywe na Berlin w grudniu 1945 roku. Operacja byla pelna nadziei, ale rowniez zmeczenia. Zimowa ofensywa miala zakonczyc rzady III Rzeszy, a niemieckie panowanie mialo stac sie wspomnieniem. Jednak niespodziewane wydarzenie zmienilo wszystko.\n");
+        typeWriterEffect("Niemcy, z pomoca swoich nowoczesnych technologii i tajnych broni, zatrzymali atak. Ale to nie wystarczylo. W odpowiedzi na zblizajacy sie upadek, III Rzesza zdecydowala sie na ostateczny krok. W wyniku desperackich dzialan, 10 grudnia 1945 roku, zrzucono bombe atomowa na Londyn, a kilka godzin pozniej na Nowy Jork, zmieniajac uklad sil na calym swiecie. Alianci, w obliczu tak ogromnej katastrofy, poddali sie, a cala zachodnia cywilizacja upada. Ziemie opanowywane przez Niemcow weszly w nowa, brutalna ere.\n");
+        typeWriterEffect("Nazywam sie " + player.name + ". Jestem, a wlasciwie byly " + player.unit + ". Ledwo co pamietam z naszej ostatniej akcji w Torgau, jedynie iskry, mroz i Brada, ktorego tutaj nie ma...\n");
+        typeWriterEffect("Minelo moze 12 godzin? Moze nawet i 14. Sam nie wiem, jestem zdezorientowany. Znalazlem sie w jakims wiezieniu? Nie jestem tu sam, jest tu duzo innych ludzi, od Niemcow, po Francuzow, a nawet i Polakow\n");
+        typeWriterEffect("Ochroniarze nosza cos podobnego do pancerzy, posiadaja maski z jednolitej plyty pancerza na twarz z otworami na czarne wizjery. Dwie rury spod tej plyty ida do jego plecaka lub jakiegos urzadzenia, ktore nosi tu prawie kazdy szkop.\n");
+        typeWriterEffect("Leze na tym niewygodnym lozku z podbitym okiem, myslac jak stąd uciec, az nagle uslyszalem alert, ktory byl transmitowany przez radia niemieckie i naglosnienie w tym obiekcie\n");
+        typeWriterEffect("Wielka chwila nadeszla. Nowy Jork, serce Ameryki, zostalo zniszczone. Nasza potege, nasza bron, zadecydowaly o losach wojny. Rzesza wykonała pierwszy, decydujacy krok ku ostatecznemu zwyciestwu.\nBomba atomowa spadla na wroga, a sily amerykanskie zostaly calkowicie zniszczone. W obliczu tej poteznej sily, nie ma juz watpliwosci – wojna zostala wygrana! Swiat, teraz pod nasza kontrola,\n bedzie zyl zgodnie z nasza wola. To my, Narod Niemiecki, ksztaltujemy przyszlosc tej planety. Zwyciestwo jest nasze!\n");
+        typeWriterEffect("Ale to nie koniec. \nZ ostatnich resztek alianckich sil na zachodzie nie zostanie nic. Nasze plany przewiduja calkowite wyeliminowanie wszelkiego oporu – zarówno na ziemiach europejskich, jak i w pozostalych zakatkach swiata.\nJuz wkrotce kazda forma buntu czy sprzeciwu wobec nowego porzadku zostanie bezwzglednie stlumiona.\n");
+
+        typeWriterEffect("Swjat, teraz pod nasza kontrola, bedzie zyl zgodnie z nasza wola.\nTo my, Narod Niemiecki, ksztaltujemy przyszlosc tej planety. Zwyciestwo jest nasze, a kazda ostatnia nadzieja na opor zostanie zmiazdzona.");
+        typeWriterEffect("O nie.. to nie moze byc prawda. Musze sie stąd jak najszybciej uciec, ale musze znalezc Brada.");
+        typeWriterEffect("1. Zawolaj straznika, blefuj, zacznij walke gdy wejdzie do celi");
+        typeWriterEffect("2. Zatkaj zlew i zalej cele woda");
+
+        cin >> choice;
+        if (choice == 1) {
+            typeWriterEffect("AY! Szczury sa pod lozkiem!");
+            typeWriterEffect("Was? Pokaz gdzie, zachowaj dystans, wiezniu.");
+            typeWriterEffect("*Straznik uklęknął i patrzy pod lozkiem, odkładając karabin na ziemię obok niego*\n");
+            typeWriterEffect("1. Weź karabin i zagroź straznikowi");
+            typeWriterEffect("2. Skocz na straznika i sproboj go przewrocic i udusic");
+
+            cin >> choice;  // Decyzja pierwsza: co robic ze straznikiem?
+
+            if (choice == 1) {
+                typeWriterEffect("*Odglos przeladowania* Nie ruszaj sie! - krzyknąłem");
+                typeWriterEffect("Oh man! Stop! Nie strzelaj!");
+                typeWriterEffect("Brad Smith!");
+                typeWriterEffect("Gdzie?");
+                typeWriterEffect("Brad Smith gdzie on jest!?");
+                typeWriterEffect("*Dowiadujesz sie gdzie jest Brad, obezwladniasz zakladnika oraz bierzesz jego uniform*");
+                typeWriterEffect("*Ciche wejscie oraz nowa sciezka odblokowana!*");
+                player.health = 100;
+            }
+            else if (choice == 2) {
+                typeWriterEffect("*Zaczyna się walka!*");
+                typeWriterEffect("Wybierz akcje, ktora chcesz wykonać:");
+                typeWriterEffect("1. Lekki atak");
+                typeWriterEffect("2. Mocny atak");
+                typeWriterEffect("3. Obrona");
+
+                cin >> choice;  // Decyzja druga: jak atakowac straznika?
+
+                if (choice == 1) {
+                    typeWriterEffect("Zadano 40 obrazen!");
+                    typeWriterEffect("Straznik cie uderza!");
+                    player.health = 30;
+                    typeWriterEffect("1. Uciekaj z celi");
+                    typeWriterEffect("2. Zaatakuj ponownie");
+
+                    cin >> choice;  // Decyzja trzecia: co dalej po ataku?
+
+                    if (choice == 1) {
+                        typeWriterEffect("Straznik cie postrzelił");
+                        player.health = 0;
+                        return;
+                    }
+                    else if (choice == 2) {
+                        typeWriterEffect("Zadano 40DMG! *Straznik zemdlał* *Podniesiono apteczkę z ekwipunku straznika +50HP*");
+                        player.health = 80;
+                    }
+                }
+                else if (choice == 2) {
+                    typeWriterEffect("Przyjąłeś cios na brzuch! (-20HP) *Zadano 80DMG straznikowi!* *Straznik zemdlał*");
+                    typeWriterEffect("Bierzesz jego ubrania i uciekasz z celi");
+                    player.health = 80;
+                }
+                else {
+                    typeWriterEffect("Straznik cie postrzelił!");
+                    player.health = 0;
+                    return;
+                }
+            }
+        }
+        else if (choice == 2) {
+            typeWriterEffect("*Zaczyna się walka!*");
+            typeWriterEffect("Wybierz akcje, ktora chcesz wykonać:");
+            typeWriterEffect("1. Lekki atak");
+            typeWriterEffect("2. Mocny atak");
+            typeWriterEffect("3. Obrona");
+
+            cin >> choice;  // Decyzja druga: jak atakowac straznika?
+
+            if (choice == 1) {
+                typeWriterEffect("Zadano 40DMG!");
+                typeWriterEffect("Straznik cie uderza! (-70HP)");
+                player.health = 30;
+                typeWriterEffect("1. Uciekaj z celi");
+                typeWriterEffect("2. Zaatakuj ponownie");
+
+                cin >> choice;  // Decyzja trzecia: co dalej po ataku?
+
+                if (choice == 1) {
+                    typeWriterEffect("Straznik cie postrzelił");
+                    player.health = 0;
+                    return;
+                }
+                else if (choice == 2) {
+                    typeWriterEffect("Zadano 40DMG! *Straznik zemdlał* *Podniesiono apteczkę z ekwipunku straznika +50HP*");
+                    player.health = 80;
+                }
+            }
+            else if (choice == 2) {
+                typeWriterEffect("Przyjąłeś cios na brzuch! (-20HP) *Zadano 80DMG straznikowi!* *Straznik zemdlał*");
+                typeWriterEffect("Bierzesz jego uniform i uciekasz z celi");
+                player.health = 80;
+            }
+            else {
+                typeWriterEffect("Straznik cie postrzelił!");
+                player.health = 0;
+                return;
+            }
+        }
+
+        if (player.health == 100) {
+            cout << "Zajmujesz sie swoja misja!";
+        }
+        else if (player.health == 80) {
+            cout << "Udalo ci sie przezyc, ale zraniony!";
         }
     }
 }
-
 int main() {
     chooseDisplayMode(); // Dodano wybór trybu przed rozpoczęciem gry
     Player player = createCharacter();
     tankPrologue(player);
 
     if (player.health <= 0) {
-        typeWriterEffect("Zostales zabity podczas ataku. Koniec gry!");
+        typeWriterEffect("K.I.A. porazka. Koniec gry!");
+        return 0;
     }
     else {
         typeWriterEffect("Zyjesz! Misja zakonczona sukcesem!");
